7d87bcb5783ef6a982164c22482df8ca54bf6b3a
MuhammmadSiraj <siraj@thoughtbox.io>
2025-11-16 17:50:46 +0300

    social icons changes

--- lib/my_site.dart
+++ lib/my_site.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:mouse_follower/mouse_follower.dart';
 import 'package:mysite/core/configs/configs.dart';
@@ -8,6 +10,8 @@ import 'package:mysite/core/theme/cubit/theme_cubit.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sizer/sizer.dart';
+
+import 'core/res/responsive.dart';
 
 class MySite extends StatelessWidget {
   const MySite({super.key});
@@ -20,36 +24,47 @@ class MySite extends StatelessWidget {
         BlocProvider<ConnectedBloc>(create: (context) => ConnectedBloc()),
       ],
       child: BlocBuilder<ThemeCubit, ThemeState>(
-          buildWhen: (previous, current) => current.isDarkThemeOn != previous.isDarkThemeOn,
-          builder: (context, state) {
-            return Sizer(builder: (context, orientation, deviceType) {
+        buildWhen: (previous, current) => current.isDarkThemeOn != previous.isDarkThemeOn,
+        builder: (context, state) {
+          return Sizer(
+            builder: (context, orientation, deviceType) {
               return MaterialApp(
                 debugShowCheckedModeBanner: false,
                 title: 'Siraj',
                 theme: AppTheme.themeData(state.isDarkThemeOn, context),
                 initialRoute: "/",
                 routes: {
-                  "/": (context) => MouseFollower(mouseStylesStack: [
-                        MouseStyle(
-                          size: const Size(7, 7),
-                          latency: const Duration(milliseconds: 25),
-                          decoration: BoxDecoration(
-                              color: Theme.of(context).primaryColor, shape: BoxShape.circle),
+                  "/": (context) => Platform.isAndroid || Platform.isIOS
+                      ? const NChecking()
+                      : MouseFollower(
+                          mouseStylesStack: [
+                            MouseStyle(
+                              size: const Size(7, 7),
+                              latency: const Duration(milliseconds: 25),
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).primaryColor,
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                            MouseStyle(
+                              size: const Size(26, 26),
+                              latency: const Duration(milliseconds: 100),
+                              visibleOnHover: false,
+                              decoration: BoxDecoration(
+                                color: Colors.transparent,
+                                shape: BoxShape.circle,
+                                border: Border.all(color: Theme.of(context).primaryColor),
+                              ),
+                            ),
+                          ],
+                          child: const NChecking(),
                         ),
-                        MouseStyle(
-                          size: const Size(26, 26),
-                          latency: const Duration(milliseconds: 100),
-                          visibleOnHover: false,
-                          decoration: BoxDecoration(
-                              color: Colors.transparent,
-                              shape: BoxShape.circle,
-                              border: Border.all(color: Theme.of(context).primaryColor)),
-                        ),
-                      ], child: const NChecking())
                 },
               );
-            });
-          }),
+            },
+          );
+        },
+      ),
     );
   }
 }
